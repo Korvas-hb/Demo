170451143ce8af87e4b9c7008990ffdc0e9b1024
Korvas <hebing7778@163.com>
2025-11-17 09:38:41 +0800

    Hero Gameplay Ability

---
+++ Source/RPGDemo/Private/AbilitySystem/Abilities/HeroGameplayAbility.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/HeroGameplayAbility.h"
+
+#include "Character/RPGDemoPlayerCharacter.h"
+#include "Controller/RPGPlayerController.h"
+
+ARPGDemoPlayerCharacter* UHeroGameplayAbility::GetHeroCharacterFromActorInfo()
+{
+	if (!CachedHeroCharacter.IsValid())
+	{
+		CachedHeroCharacter = CastChecked<ARPGDemoPlayerCharacter>(GetAvatarActorFromActorInfo());
+	}
+	return CachedHeroCharacter.IsValid() ? CachedHeroCharacter.Get() : nullptr;
+}
+
+ARPGPlayerController* UHeroGameplayAbility::GetHeroControllerFromActorInfo()
+{
+	if (!CachedHeroController.IsValid())
+	{
+		CachedHeroController = CastChecked<ARPGPlayerController>(CurrentActorInfo->PlayerController);
+	}
+	return CachedHeroController.IsValid() ? CachedHeroController.Get() : nullptr;
+}
+
+UHeroCombatComponent* UHeroGameplayAbility::GetHeroCombatComponentFromActorInfo() const
+{
+	if (CachedHeroCharacter.IsValid())
+	{
+		return CachedHeroCharacter->GetHeroCombatComponent();
+	}
+	return nullptr;
+}
---
+++ Source/RPGDemo/Public/AbilitySystem/Abilities/HeroGameplayAbility.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/RPGGameplayAbility.h"
+#include "HeroGameplayAbility.generated.h"
+
+class UHeroCombatComponent;
+class ARPGPlayerController;
+class ARPGDemoPlayerCharacter;
+
+UCLASS()
+class RPGDEMO_API UHeroGameplayAbility : public URPGGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	// 自己GA里面提供三个函数分别可以获取自己Owner的Character，Controller和其自己的战斗系统.
+	UFUNCTION(BlueprintPure, Category="Abilities")
+	ARPGDemoPlayerCharacter* GetHeroCharacterFromActorInfo();
+
+	UFUNCTION(BlueprintPure, Category="Abilities")
+	ARPGPlayerController* GetHeroControllerFromActorInfo();
+
+	UFUNCTION(BlueprintPure, Category="Abilities")
+	UHeroCombatComponent* GetHeroCombatComponentFromActorInfo() const;
+
+private:
+	TWeakObjectPtr<ARPGDemoPlayerCharacter> CachedHeroCharacter;
+	TWeakObjectPtr<ARPGPlayerController> CachedHeroController;
+};
